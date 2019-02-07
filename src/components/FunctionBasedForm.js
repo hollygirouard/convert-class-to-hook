21dac9ffe684ec68f060885fc00f8cdbe276e1ed
holly <hollyelizabethwhite@gmail.com>
2019-02-07 15:49:04 -0700

    solution code

--- src/components/FunctionBasedForm.js
+++ src/components/FunctionBasedForm.js
@@ -1,12 +1,57 @@
 import React, { useState } from 'react'
+import {
+  Form, FormGroup, Input,
+  Label, Col, Button,
+} from 'reactstrap';
 
 const FunctionBasedForm = () => {
-  const [ email, setEmail] = useState(null);
-  const [ password, setPassword] = useState(null);
-  const [ attemptCount, setAttemptedCount] = useState(0);
-  return (
-    <h1>Function Based Form</h1>
-  )
-}
+  const [ email, setEmail] = useState('');
+  const [ password, setPassword] = useState('');
 
-export default FunctionBasedForm
+  const handleSubmit = event => {
+    event.preventDefault();
+    setEmail(email);
+    setPassword(password);
+    console.log(email);
+    console.log(password);
+  }
+
+  return (
+    <Form onSubmit={ handleSubmit }>
+      <h1>Function-Based Form</h1>
+      <FormGroup row>
+        <Label for="exampleEmail" sm={ 2 }>Email</Label>
+        <Col sm={ 8 }>
+          <Input
+            type="email"
+            name="email"
+            id="exampleEmail"
+            placeholder="email"
+            value={ email }
+            onChange={ event => setEmail(event.target.value) }
+          />
+        </Col>
+      </FormGroup>
+      <FormGroup row>
+        <Label for="examplePassword" sm={ 2 }>Password</Label>
+        <Col sm={ 8 }>
+          <Input
+            type="password"
+            name="password"
+            id="examplePassword"
+            placeholder="password"
+            value={ password }
+            onChange={ event => setPassword(event.target.value) }
+          />
+        </Col>
+      </FormGroup>
+      <FormGroup check row>
+        <Col sm={ { size: 10, offset: 8 } }>
+          <Button>Submit</Button>
+        </Col>
+      </FormGroup>
+    </Form>
+  )
+};
+
+export default FunctionBasedForm;
